ad52a571b6b277922a3a8ea3ee8f2acc447c5720
Frank <frankk.cerny@gmail.com>
2016-06-08 15:40:21 -0400

    Daily Update 6-8-16
    Added:
    -Km/liter accessability
    -basic user validation

--- app/src/main/java/com/example/frank/myappnamepending/CalculateScreen.java
+++ app/src/main/java/com/example/frank/myappnamepending/CalculateScreen.java
@@ -8,9 +8,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.*;
-import java.io.*;
-
 import org.w3c.dom.Text;
 
 public class CalculateScreen extends AppCompatActivity {
@@ -29,6 +26,13 @@ public class CalculateScreen extends AppCompatActivity {
     private TextView Output;
     private Button btnReset;
     private String value;
+    private TextView error;
+    private Button UnitSwitch;
+    private String Unit;
+    private TextView CarEfficiency;
+    private TextView TextGasPrice;
+    private TextView Distance;
+    private String currentUnit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +47,38 @@ public class CalculateScreen extends AppCompatActivity {
         Output = (TextView)findViewById(R.id.savedMoney);
         btnCalculate = (Button)findViewById(R.id.buttonCalculate);
         btnReset = (Button)findViewById(R.id.btn_reset);
+        error = (TextView)findViewById(R.id.errorMessage);
+        UnitSwitch = (Button)findViewById(R.id.unitSwitch);
+        CarEfficiency = (TextView)findViewById(R.id.textViewMPG);
+        TextGasPrice = (TextView)findViewById(R.id.textViewPrice);
+        Distance = (TextView)findViewById(R.id.textViewDistance);
+
+
+        // This ensures that other countries can use the app, this changes all prompts
+        // into a KM and liter based system, the button acts as a toggle, only EU for now
+
+        UnitSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                currentUnit = UnitSwitch.getText().toString();
+
+                if (currentUnit.equalsIgnoreCase("To INT")) {
+                    UnitSwitch.setText("To US");
+                    currentUnit = "INT";
+                    TextGasPrice.setText("Gas Price Per Liter");
+                    CarEfficiency.setText("KM/Liter of Main car");
+                    Distance.setText("Distance in KM");
+                }
+                else {
+                    UnitSwitch.setText("To INT");
+                    currentUnit = "US";
+                    TextGasPrice.setText("Gas Price Per Gallon");
+                    CarEfficiency.setText("MPG of Main Car");
+                    Distance.setText("Distance in Miles");
+                }
+            }
+        });
 
         // All this executes when the button is pressed, it checks to makes sure there
         // is input in each box then it grabs it and puts it in the variables
@@ -51,19 +87,37 @@ public class CalculateScreen extends AppCompatActivity {
         btnCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (GasPrice.getText().toString().length() > 0) {
+
+                if (GasPrice.getText().toString().length() > 0 && MilesPerGallon.getText().toString().length() > 0
+                        && distance.getText().toString().length() > 0) {
+
+                    error.setText("");
                     numGasPrice = Double.parseDouble(GasPrice.getText().toString());
-                }
-                if (MilesPerGallon.getText().toString().length() > 0) {
                     numMPG = Double.parseDouble(MilesPerGallon.getText().toString());
-                }
-                if (distance.getText().toString().length() > 0) {
                     numDistance = Double.parseDouble(distance.getText().toString());
+
+                    moneySaved = (numDistance / numMPG) * numGasPrice;
+                    Double.toString(moneySaved);
+
+                    // This converts the output saying to Euros for better accesiblity of
+                    // international countries
+
+                    if (currentUnit == "US") {
+                        value = String.format("You Saved $ %.2f", moneySaved);
+                    }
+                    else {
+                        value = String.format("You Saved € %.2f", moneySaved);
+                    }
+                    Output.setText(value);
                 }
-                moneySaved = (numDistance / numMPG) * numGasPrice;
-                Double.toString(moneySaved);
-                value = String.format("You Saved $ %.2f", moneySaved);
-                Output.setText(value);
+
+                // This is a basic validation to make sure that the user entered values
+                // in all three boxes, if not it will promt them to retry and enter values
+
+                else {
+                    error.setText("Error! Please enter values in all the inputs and recalculate!");
+                }
+
             }
         });
 
@@ -80,6 +134,7 @@ public class CalculateScreen extends AppCompatActivity {
                 MilesPerGallon.setText("");
                 distance.setText("");
                 Output.setText("");
+                error.setText("");
             }
         });
     }
